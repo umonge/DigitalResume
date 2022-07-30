feab84164fa98dac57a5277687df157d58c9efc9
Ulises Monge C <umonge@vlacacademy.com>
2022-07-29 21:51:44 -0600

    Se inicializa el repositorio de la hoja de vida

---
+++ javascript/01.js
@@ -0,0 +1,21 @@
+let i=5;
+let j=8;
+
+j=4;
+i=9;
+
+if(i<j){
+ console.log( i+' es menor que '+j)
+} else if(j<i){
+console.log(j+' es menor que '+i)
+}else {
+console.log(i+'es igual a: '+j)
+}
+
+if(i>j){
+    console.log( i+' es mayor que '+j)
+   } else if(j>i){
+   console.log(j+' es mayor que '+i)
+   }else {
+   console.log(i+'es igual a: '+j)
+   }
